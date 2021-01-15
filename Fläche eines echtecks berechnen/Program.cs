3fb220aeca5f3e4ca4ac8b92ce5f1bf4b9a5b48d
Leonie Roth <leonieroth@Leonies-MBP.local>
2021-01-15 12:03:43 +0100

    aufgaben

---
+++ Fläche eines echtecks berechnen/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fläche_eines_echtecks_berechnen
+{
+    static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FlächeEinesRechtecksBenennen());
+        }
+    }
+}
